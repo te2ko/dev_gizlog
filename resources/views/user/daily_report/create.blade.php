a51e29f38e34b6c1e9966a04e6537da348be0c8b
teppei <yamamutaisou@gmail.com>
2019-09-25 18:56:10 +0900

    'formファザードのメソッドを変更　inputをdate,textに変更'

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,13 +5,13 @@
  <div class="container">
     {!! Form::open(['route' => 'daily_report.store']) !!}
     <div class="form-group form-size-small{{ $errors->has('reporting_time') ? ' has-error' : '' }}">
-      {!! Form::input('date','reporting_time', Carbon::now()->format('Y-m-d'),['class' => 'form-control']) !!}
+      {!! Form::date('reporting_time', Carbon::now()->format('Y-m-d'),['class' => 'form-control']) !!}
       @if ($errors->has('reporting_time'))
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       @endif
     </div>
     <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-      {!! Form::input('text','title',null,['class' => 'form-control', 'placeholder' => 'Title']) !!}
+      {!! Form::text('title',null,['class' => 'form-control', 'placeholder' => 'Title']) !!}
       @if ($errors->has('title'))
         <span class="help-block">{{ $errors->first('title') }}</span>
       @endif
--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -6,13 +6,13 @@
  <div class="container">
   {!! Form::open(['route' => ['daily_report.update', $report->id], 'method' => 'PUT']) !!}
     <div class="form-group form-size-small{{ $errors->has('reporting_time') ? ' has-error' : '' }}">
-      {!! Form::input('date','reporting_time', $report->reporting_time->format('Y-m-d'),['class' => 'form-control']) !!}
+      {!! Form::date('reporting_time', $report->reporting_time->format('Y-m-d'),['class' => 'form-control']) !!}
       @if ($errors->has('reporting_time'))
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       @endif
     </div>
     <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-      {!! Form::input('text','title', $report->title,['class' => 'form-control', 'placeholder' => 'Title']) !!}
+      {!! Form::text('title', $report->title,['class' => 'form-control', 'placeholder' => 'Title']) !!}
       @if ($errors->has('title'))
         <span class="help-block">{{ $errors->first('title') }}</span>
       @endif
