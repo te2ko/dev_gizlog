79ba337ace0a1df3af63608009de108baecebca4
teppei <yamamutaisou@gmail.com>
2019-09-26 14:30:04 +0900

    'バリデーションのメッセージ表示の条件分岐撤廃'

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -6,21 +6,15 @@
     {!! Form::open(['route' => 'daily_report.store']) !!}
     <div class="form-group form-size-small{{ $errors->has('reporting_time') ? ' has-error' : '' }}">
       {!! Form::date('reporting_time', Carbon::now()->format('Y-m-d'),['class' => 'form-control']) !!}
-      @if ($errors->has('reporting_time'))
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
-      @endif
     </div>
     <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
       {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-      @if ($errors->has('title'))
         <span class="help-block">{{ $errors->first('title') }}</span>
-      @endif
     </div>
     <div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
       {!! Form::textarea('content', null, ['class' => 'form-control','placeholder' => 'Content']) !!}
-      @if ($errors->has('content'))
       <span class="help-block">{{ $errors->first('content') }}</span>
-      @endif
     </div>
     {!! Form::submit('add', ['class' => 'btn btn-success pull-right']) !!}
     {!! Form::close() !!}
--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -7,21 +7,16 @@
   {!! Form::open(['route' => ['daily_report.update', $report->id], 'method' => 'PUT']) !!}
     <div class="form-group form-size-small{{ $errors->has('reporting_time') ? ' has-error' : '' }}">
       {!! Form::date('reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
-      @if ($errors->has('reporting_time'))
+
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
-      @endif
     </div>
     <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
       {!! Form::text('title', $report->title, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-      @if ($errors->has('title'))
         <span class="help-block">{{ $errors->first('title') }}</span>
-      @endif
     </div>
     <div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
       {!! Form::textarea('content',$report->content, ['class' => 'form-control','placeholder' => 'Content']) !!}
-      @if ($errors->has('content'))
         <span class="help-block">{{ $errors->first('content') }}</span>
-      @endif
     </div>
     {!! Form::submit('Update', ['class' => 'btn btn-success pull-right']) !!}
   {!! Form::close() !!}
