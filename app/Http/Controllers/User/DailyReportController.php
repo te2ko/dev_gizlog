5d03b965e9c3ee0154cfb36a824a5199be9c3260
teppei <yamamutaisou@gmail.com>
2019-09-27 16:27:56 +0900

    'phpdocとメソッドの内容に合わせた'

--- app/Http/Controllers/User/DailyReportController.php
+++ app/Http/Controllers/User/DailyReportController.php
@@ -43,7 +43,7 @@ class DailyReportController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\User\DailyReportRequest  $request
      * @return \Illuminate\Http\Response
      */
     public function store(DailyReportRequest $request)
