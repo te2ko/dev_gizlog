12b8aa0002e5afe82e7cb363784a47831624e4fd
teppei <yamamutaisou@gmail.com>
2019-10-03 14:39:22 +0900

    'テーブル作成'

---
+++ database/migrations/2019_10_03_033011_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('tag_category_id');
+            $table->char('title', 100);
+            $table->text('content');
+            $table->timestamps();
+            $table->softdeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
