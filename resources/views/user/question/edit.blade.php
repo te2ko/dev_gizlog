cdd1b17a5e3b35736e96ccf765035395eebf33eb
teppei <yamamutaisou@gmail.com>
2019-10-10 13:43:23 +0900

    '編集画面の実装'

--- resources/views/user/question/edit.blade.php
+++ resources/views/user/question/edit.blade.php
@@ -1,28 +1,25 @@
 @extends ('common.user')
 @section ('content')
-
 <h1 class="brand-header">質問編集</h1>
 
 <div class="main-wrap">
   <div class="container">
-    <form>
-      <div class="form-group">
-        <select name='tag_category_id' class = "form-control selectpicker form-size-small" id ="pref_id">
-          <option value=""></option>
-            <option value= ""></option>
-        </select>
-        <span class="help-block"></span>
+    {!! Form::open(['route' => ['question.confirm']]) !!}
+      <div class="form-group {{ !empty($errors->first('tag_category_id')) ? 'has-error' : '' }}">
+      {!! Form::select('' => 'tag_category_id', ['Select category', 1 => 'FRONT', 2 => 'BACK', 3 => 'INFRA', 4 => 'OTHERS'], $input->tag_category_id, ['class' => 'form-control selectpicker form-size-small', 'id' => 'pref_id']) !!}
+        <span class="help-block">{{ $errors->first('tag_category_id') }}</span>
       </div>
-      <div class="form-group">
-        <input class="form-control" placeholder="title" name="title" type="text" value="">
-        <span class="help-block"></span>
+      <div class="form-group {{ !empty($errors->first('title')) ? 'has-error' : '' }}">
+        <input class="form-control" placeholder="title" name="title" type="text" value="{{ $input->title }}">
+        <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
-      <div class="form-group">
-        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10"></textarea>
-        <span class="help-block"></span>
+      <div class="form-group {{ !empty($errors->first('content')) ? 'has-error' : '' }}">
+        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10">{!! nl2br(e($report->content)) !!}</textarea>
+        <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
+      <input name="questionId" type="hidden" value="{{ $questionId }}">
       <input name="confirm" class="btn btn-success pull-right" type="submit" value="update">
-    </form>
+    {!! Form::close() !!}
   </div>
 </div>
 
