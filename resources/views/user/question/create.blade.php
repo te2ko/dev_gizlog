07989ced829241b98bc3c1ec412511d644295376
teppei <yamamutaisou@gmail.com>
2019-10-10 13:44:46 +0900

    '新規作成画面の実装'

--- resources/views/user/question/create.blade.php
+++ resources/views/user/question/create.blade.php
@@ -4,26 +4,22 @@
 <h2 class="brand-header">質問投稿</h2>
 <div class="main-wrap">
   <div class="container">
-    <form>
-      <div class="form-group">
-        <select name='tag_category_id' class = "form-control selectpicker form-size-small" id="pref_id">
-          <option value="">Select category</option>
-            <option value= ""></option>
-        </select>
-        <span class="help-block"></span>
+    {!! Form::open(['route' => 'question.confirm']) !!}
+      <div class="form-group {{ !empty($errors->first('tag_category_id')) ? 'has-error' : '' }}">
+          {!! Form::select('tag_category_id', ['' => 'Select category', 1 => 'FRONT', 2 => 'BACK', 3 => 'INFRA', 4 => 'OTHERS'], !empty(old('tag_category_id')) ? old('tag_category_id') : 'Select category', ['class' => 'form-control selectpicker form-size-small', 'id' => 'pref_id']) !!}
+         <span class="help-block">{{ $errors->first('tag_category_id') }}</span>
       </div>
-      <div class="form-group">
-        <input class="form-control" placeholder="title" name="title" type="text">
-        <span class="help-block"></span>
+      <div class="form-group {{ !empty($errors->first('title')) ? 'has-error' : '' }}">
+          {!! Form::text('title', old('title'), ['class' => 'form-control', 'placeholder' => 'title']) !!}
+          <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
-      <div class="form-group">
-        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10"></textarea>
-        <span class="help-block"></span>
+      <div class="form-group {{ !empty($errors->first('content')) ? 'has-error' : '' }}">
+          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'placeholder' => 'Please write down your question here...']) !!}
+          <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
       <input name="confirm" class="btn btn-success pull-right" type="submit" value="create">
-    </form>
+    {!! Form::close() !!}
   </div>
 </div>
-
 @endsection
 
