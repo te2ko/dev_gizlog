d4b86fdd8f1a06c63a4c804bef4b566a4c0e43b6
teppei <yamamutaisou@gmail.com>
2019-10-10 13:46:51 +0900

    'show画面の実装'

--- resources/views/user/question/show.blade.php
+++ resources/views/user/question/show.blade.php
@@ -1,12 +1,11 @@
 @extends ('common.user')
 @section ('content')
-
 <h1 class="brand-header">質問詳細</h1>
 <div class="main-wrap">
   <div class="panel panel-success">
     <div class="panel-heading">
-      <img src="" class="avatar-img">
-      <p>&nbsp;さんの質問&nbsp;&nbsp;(&nbsp;&nbsp;)</p>
+      <img src="{{ $info->avatar ? $info->avatar : 'https://www.u-stat.net/images/site_img/pimg.png' }}" class="avatar-img">
+      <p>{{ $info->name }}&nbsp;さんの質問&nbsp;&nbsp;({{ $categoryName->name }})</p>
       <p class="question-date"></p>
     </div>
     <div class="table-responsive">
@@ -14,43 +13,45 @@
         <tbody>
           <tr>
             <th class="table-column">Title</th>
-            <td class="td-text"></td>
+            <td class="td-text">{{ $info->title }}</td>
           </tr>
           <tr>
             <th class="table-column">Question</th>
-            <td class='td-text'></td>
+            <td class='td-text'>{{ $info->content }}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
-    <div class="comment-list">
-        <div class="comment-wrap">
-          <div class="comment-title">
-            <img src="" class="avatar-img">
-            <p></p>
-            <p class="comment-date"></p>
+    @foreach ($commentInfos as $commentInfo)
+      <div class="comment-list">
+          <div class="comment-wrap">
+            <div class="comment-title">
+              <img src="{{ $commentInfo->avatar ? $commentInfo->avatar : 'https://www.u-stat.net/images/site_img/pimg.png' }}" class="avatar-img">
+              <p>{{ $commentInfo->name }}</p>
+              <p class="comment-date">{{ $commentInfo->created_at }}</p>
+            </div>
+            <div class="comment-body">{!! nl2br(e($commentInfo->comment)) !!}</div>
           </div>
-          <div class="comment-body"></div>
-        </div>
-    </div>
-  <div class="comment-box">
-    <form>
-      <input name="user_id" type="hidden" value="">
-      <input name="question_id" type="hidden" value="">
-      <div class="comment-title">
-        <img src="" class="avatar-img"><p>コメントを投稿する</p>
       </div>
-      <div class="comment-body">
+    @endforeach
+  <div class="comment-box">
+    {!! Form::open(['route' => 'question.commentcreate']) !!}
+      <input name="user_id" type="hidden" value="{{ $userId }}">
+      <input name="question_id" type="hidden" value="{{ $info->question_id }}">
+      <div class="comment-title">
+        <img src="{{ $avatar ? $avatar : 'https://www.u-stat.net/images/site_img/pimg.png'}}" class="avatar-img"><p>コメントを投稿する</p>
+      </div>
+      <div class="comment-body {{ !empty($errors->first('comment')) ? 'has-error' : '' }}">
         <textarea class="form-control" placeholder="Add your comment..." name="comment" cols="50" rows="10"></textarea>
-        <span class="help-block"></span>
+        <span class="help-block">{{ $errors->first('comment') }}</span>
       </div>
       <div class="comment-bottom">
         <button type="submit" class="btn btn-success">
           <i class="fa fa-pencil" aria-hidden="true"></i>
         </button>
       </div>
-    </form>
+    {!! Form::close() !!}
   </div>
 </div>
 @endsection
