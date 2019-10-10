c0a6d07d865581afc672bd9cfbfe574f1bf78ea0
teppei <yamamutaisou@gmail.com>
2019-10-10 13:55:06 +0900

    'commentモデルの実装'

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'question_id',
+        'comment'
+    ];
+
+    public function scopeFetchComments($query, $questionId)
+    {
+        return $query->join('users', 'comments.user_id', '=', 'users.id')
+                     ->join('questions', 'comments.question_id', '=', 'questions.id')
+                     ->where('questions.id', '=', $questionId)
+                     ->select('users.name', 'users.avatar', 'comments.comment', 'comments.created_at')
+                     ->orderBy('comments.created_at', 'desc');
+    }
 }
