cf1cacb30346b1acb14440f3b41a27d884b4f364
teppei <yamamutaisou@gmail.com>
2019-10-11 15:28:14 +0900

    'formでバグが起きていたので修正原因は記述ミス'

--- resources/views/user/question/edit.blade.php
+++ resources/views/user/question/edit.blade.php
@@ -6,7 +6,7 @@
   <div class="container">
     {!! Form::open(['route' => ['question.confirm']]) !!}
       <div class="form-group {{ !empty($errors->first('tag_category_id')) ? 'has-error' : '' }}">
-      {!! Form::select('' => 'tag_category_id', ['Select category', 1 => 'FRONT', 2 => 'BACK', 3 => 'INFRA', 4 => 'OTHERS'], $input->tag_category_id, ['class' => 'form-control selectpicker form-size-small', 'id' => 'pref_id']) !!}
+      {!! Form::select('tag_category_id', ['' => 'Select category', 1 => 'FRONT', 2 => 'BACK', 3 => 'INFRA', 4 => 'OTHERS'], $input->tag_category_id, ['class' => 'form-control selectpicker form-size-small', 'id' => 'pref_id']) !!}
         <span class="help-block">{{ $errors->first('tag_category_id') }}</span>
       </div>
       <div class="form-group {{ !empty($errors->first('title')) ? 'has-error' : '' }}">
@@ -14,7 +14,7 @@
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group {{ !empty($errors->first('content')) ? 'has-error' : '' }}">
-        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10">{!! nl2br(e($report->content)) !!}</textarea>
+        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10">{!! nl2br(e($input->content)) !!}</textarea>
         <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
       <input name="questionId" type="hidden" value="{{ $questionId }}">
