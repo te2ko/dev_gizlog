9e8217aef358543a00d4dc2183fb3a05c64fd411
teppei <yamamutaisou@gmail.com>
2019-10-15 17:30:18 +0900

    'バリデーションのリネームしたときにコントローラーのuseしてるモデルの名前変更忘れの修正'

--- app/Http/Controllers/User/QuestionController.php
+++ app/Http/Controllers/User/QuestionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\http\Requests\User\QuestionsRequest;
+use App\http\Requests\User\QuestionRequest;
 use App\http\Requests\User\CommentRequest;
 use App\http\Requests\User\SearchRequest;
 use App\http\Controllers\Controller;
